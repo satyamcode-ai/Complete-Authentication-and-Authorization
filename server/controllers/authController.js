9192001ab0b2ec20232b3200c7b45dd12bb3139a
satyam AI <satyamweb2004@gmail.com>
2025-08-29 01:42:41 +0530

    change httponly

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -80,7 +80,7 @@ export const login = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         res.cookie('token', token, {
-            httpOnly: false,
+            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
