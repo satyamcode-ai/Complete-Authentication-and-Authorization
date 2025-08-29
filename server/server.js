90d328379093eef2d9d61acebcff5e65a454a849
satyam AI <satyamweb2004@gmail.com>
2025-08-29 10:57:09 +0530

    url update

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());    
 
 app.use(cors({
-      origin:'https://complete-authentication-and-authori.vercel.app/',
+      origin:'https://complete-authentication-and-authori.vercel.app',
       credentials: true,   
 }));
 
