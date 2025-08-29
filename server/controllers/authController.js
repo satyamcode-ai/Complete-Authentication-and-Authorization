334324560838974039b22d09cb3ebf326a3867ff
satyam AI <satyamweb2004@gmail.com>
2025-08-29 10:42:02 +0530

    changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,8 +32,8 @@ export const register = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict',
+            secure: true,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
@@ -81,8 +81,8 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict',
+            secure: true,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
@@ -100,8 +100,8 @@ export const logout = (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict'
+            secure: true,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
         });
 
         res.status(200).json({ success: true, message: "Logout successful" });
